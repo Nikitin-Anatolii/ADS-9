1b2e97c3451774b725b3085ad30f4c0d15766a88
Nikitin-Anatolii <99128452+Nikitin-Anatolii@users.noreply.github.com>
2022-05-28 22:23:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,21 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
-
+#include <stdlib.h>
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> Tree;
+  std::ifstream fin(filename);
+  std::string str = "";
+  while (!fin.eof()) {
+    char a = fin.get();
+    if (a >= 'A' && a <= 'Z') {
+      str += tolower(a);
+    } else if (a >= 'a' && a <= 'z') {
+      str += a;
+    } else {
+      binTree.add(str);
+      str = "";
+    }
+  }
+  return Tree;
 }
